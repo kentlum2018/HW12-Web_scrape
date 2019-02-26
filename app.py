59e5faae0f4af8dac880d0e506cadf190065266c
kentlum2018 <43477335+kentlum2018@users.noreply.github.com>
2019-02-25 21:23:09 -0800

    revisions
    revised for bugs

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+#from scrape_mars import scrape
 import scrape_mars
+
+
+
+
+
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_mars"] = "mongodb://localhost:27017/mars_db"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 mongo = PyMongo(app)
 
 # Or set inline
@@ -15,15 +22,15 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     #create a list to store collection
-    listings = mongo.db.mars_table.find_one()
-    return render_template("index.html", listings=listings)
+    mars_raw = mongo.db.mars_table.find_one()
+    return render_template("index.html", mars_raw=mars_raw)
 
 
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_mars.scrape()
-    listings.update({}, listings_data, upsert=True)
+def scrape():
+    mars_db = mongo.db.mars_table
+    mars_dict = scrape_mars.scrape()
+    mars_db.update({}, mars_dict, upsert=True)
     return redirect("/", code=302)
 
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -5,115 +5,132 @@ from splinter import Browser
 import time
 
 # Func to start splinter and assign Chrome as browser
-executable_path = {'executable_path': 'chromedriver.exe'}
+# # executable_path = {'executable_path': 'chromedriver.exe'}
 # I changed the CHROMEDRIVER!!! as per Ernest
+executable_path = {"executable_path": "chromedriver"}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
 #create SCRAPE func for html page
-def scrape()
-    browser = init_browser()
+def scrape():
+    # browser = init_browser()
 
-#create dict for holding all info
+    #create dict for holding all info
     mars_raw={}    
-## Mars News
+    ## Mars News
 
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    time.sleep(3)
+    #scrape headline
+    html=browser.html
+    news_soup = bs(browser.html, 'lxml')
+    # print(news_soup.prettify())
+    time.sleep(2)
+    #to pause 
+    news_title = news_soup.find(class_='content_title', string=True)
+    #print(news_title)
+    time.sleep(2)
+    #to pause
+    news_p = news_soup.find(class_='article_teaser_body', string=True)
+    # print(news_p)
 
-#scrape headline
-html=browser.html
-news_soup = bs(browser.html, 'lxml')
-# print(news_soup.prettify())
-time.sleep(2)
-#to pause 
-news_title = news_soup.find(class_='content_title', string=True)
-#print(news_title)
-time.sleep(2)
-#to pause
-news_p = news_soup.find(class_='article_teaser_body', string=True)
-# print(news_p)
+    #add to dict
+    mars_raw['cur_news_title']=news_title
+    mars_raw['cur_news_p']=news_p
 
+    #scrape images
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    base_url='https://www.jpl.nasa.gov'
+    browser.visit(url)
+    #click thru to get to final image
+    browser.click_link_by_partial_text('FULL IMAGE')
+    time.sleep(2)
+    browser.click_link_by_partial_text('more info')
 
-#add to dict
-mars_raw['cur_news_title']=news_title
-mars_raw['cur_news_p']=news_p
+    #Create a Beautiful Soup object
+    soup_img = bs(browser.html, 'lxml')
 
+    #scrape final image
+    news_img =soup_img.find(class_="main_image")
+    # print(news_img)
 
-#scrape images
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-base_url='https://www.jpl.nasa.gov'
-browser.visit(url)
-#click thru to get to final image
-browser.click_link_by_partial_text('FULL IMAGE')
-time.sleep(2)
-browser.click_link_by_partial_text('more info')
+    #full image URL
+    featured_image_URL= base_url + news_img['src']
+    #print(featured_image_URL)
 
-#Create a Beautiful Soup object
-soup_img = bs(browser.html, 'lxml')
+    #add to dict
+    main_image=news_img
+    # mars_raw[image_1]=news_img
+    # mars_raw[image_caption]=featured_image_URL
+    image_txt=featured_image_URL
 
-#scrape final image
-news_img =soup_img.find(class_="main_image")
-# print(news_img)
+    ### Mars Weather
+    url = 'https://twitter.com/marswxreport?lang=en'
+    browser.visit(url)
 
-#full image URL
-featured_image_URL= base_url + news_img['src']
-print(featured_image_URL)
+    #Create a Beautiful Soup object
+    soup_wea = bs(browser.html, 'lxml')
+    # print(soup_wea.prettify())
 
-#add to dict
-mars_raw[image_1]=news_img
-mars_raw[image_caption]=featured_image_URL
+    #scrape for twitter text
+    twe_txt =soup_wea.find(class_="tweet-text")
+    #print(twe_txt)
 
-### Mars Weather
-url = 'https://twitter.com/marswxreport?lang=en'
-browser.visit(url)
+     #add to dict
+    # mars_raw[wea_txt]=twe_txt
+    wea_txt=twe_txt
+    ### Mars Facts
 
-#Create a Beautiful Soup object
-soup_wea = bs(browser.html, 'lxml')
-# print(soup_wea.prettify())
+    url = 'http://space-facts.com/mars/'
+    browser.visit(url)
 
-#scrape for twitter text
-twe_txt =soup_wea.find(class_="tweet-text")
-#print(twe_txt)
+    #scrape table
+    tables = pd.read_html(url)
+    #format
+    df = tables[0]
+    df.columns = ['Component', 'Measurement']
+    #     df.head()
+    html_table = df.to_html(index=False)
+    #     html_table
+    html_table= html_table.replace('\n', '')
+    #     html_table
 
-#add to dict
-mars_raw[wea_txt]=twe_txt
-
-### Mars Facts
-
-url = 'http://space-facts.com/mars/'
-browser.visit(url)
-
-#scrape table
-tables = pd.read_html(url)
-#format
-df = tables[0]
-df.columns = ['Component', 'Measurement']
-df.head()
-html_table = df.to_html(index=False)
-html_table
-html_table= html_table.replace('\n', '')
-html_table
-
-### Mars Hemispheres
-hemis_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-browser.visit(hemis_url)
-#use for loop to extract all the 4 hemispheres in a list; use the list then to pull all the 4 images 
-hemisphere_image_urls = []
-browser.visit(hemis_url)
-hemis_soup = bs(browser.html, 'lxml')
-hemis_query = hemis_soup.find_all(class_='item')
-for result in hemis_query:
-    
+    ### Mars Hemispheres
+    hemis_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemis_url)
-    browser.click_link_by_partial_text(result.h3.text)
-    hemis_temp_soup = bs(browser.html, 'lxml')
-    
-    hemi_name = result.h3.text.replace(' Enhanced', '')
-    hemi_img_url = hemis_temp_soup.find(target='_blank')['href']
-    
-    hemisphere_image_urls.append({'title':hemi_name, 'img_url':hemi_img_url})
-#add to dict
-mars_raw[hemi_img]=hemisphere_image_urls
+    #use for loop to extract all the 4 hemispheres in a list
+    # use the list then to pull all the 4 images 
+    hemisphere_image_urls = []
+    browser.visit(hemis_url)
+    hemis_soup = bs(browser.html, 'lxml')
+    hemis_query = hemis_soup.find_all(class_='item')
+    for result in hemis_query:
+        browser.visit(hemis_url)
+        browser.click_link_by_partial_text(result.h3.text)
+        hemis_temp_soup = bs(browser.html, 'lxml')
+        hemi_name = result.h3.text.replace(' Enhanced', '')
+        hemi_img_url = hemis_temp_soup.find(target='_blank')['href']
+        hemisphere_image_urls.append({'title':hemi_name, 'img_url':hemi_img_url})
+        #add to dict
+        # mars_raw[hemi_img]=hemisphere_image_urls
+        # hemi_img1=hemisphere_image_urls[0]
+        # hemi_img2=hemisphere_image_urls[1]
+        # hemi_img3=hemisphere_image_urls[2]
+        # hemi_img4=hemisphere_image_urls[3]
 
-return mars_raw
+    mars_raw = dict (
+        cur_news_title =news_title,
+        cur_news_p =news_p,
+        # image_1 =news_img,
+        image_1=main_image,
+        image_caption = image_txt,
+        tweet =wea_txt,
+        html_table =html_table,
+        hemi1 =hemisphere_image_urls
+        # hemi2 =hemisphere_image_urls,
+        # hemi3 =hemisphere_image_urls,
+        # hemi4 =hemisphere_image_urls  
+    )
+
+    return mars_raw
